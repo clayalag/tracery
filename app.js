8c32635c08622f72e97838b3dee59a1c35853a5b
clayalag <christopher.ayala@upr.edu>
2021-05-03 14:04:41 -0400

    add some fixes

--- app.js
+++ app.js
@@ -1,34 +1,42 @@
-console.log("app");
+let tweets = []
+function generate() {
+    /*var data = {
+        "start": ["#tweet#"],
+        "tweet": ["#NP# #VP#"],
+        "NP": ["#Pronoun#", "#Noun#", "#Determiner# #Nominal#"],
+        "Nominal": ["#Noun# #Nominal#", "#Noun#"],
+        "VP": ["#Verb#", "#Verb# #NP#", "#Verb# #PP# #NP#", "#Verb# #PP#"],
+        "PP": ["#Preposition# #NP#"],
+        "Verb": ["is", "play", "played", "playing", "like", "liked", "likes", "need", "needed", "needs", "want", "wanted", "wants", "fly", "flying", "flies", "jump", "jumping", "jumps", "run", "running", "runs", "dance", "dancing", "dances"],
+        "Pronoun": ["me", "I", "you", "it", "he", "she"],
+        "Noun": ["John Keating", "Bob Harris", "Bruce Wayne", "John Constantine", "Gotham", "Asgard", "Diagon Alley", "Narnia"],
+        "Determiner": ["the", "a", "an", "this", "these", "that", "there"],
+        "Preposition": ["from", "to", "on", "near", "with"]
+    }*/
 
-$(document).ready(function() {
-
-    function loadGrammar(name) {
-        $("#output").html("");
-
-        var grammar = tracery.createGrammar(grammars[name]);
-        $("#grammar").html(grammar.toText());
-
-        for (var i = 0; i < 10; i++) {
-
-            var s = grammar.flatten("#origin#");
-            console.log(s);
-            var div = $("<div/>", {
-                class : "outputSample",
-                html : s
-            });
-
-            $("#output").append(div);
-
-        }
-
+    var data = {
+    	"start": ["#NP# #VP#."],
+    	"NP": ["#Det# #N#", "#Det# #N# that #VP#", "#Det# #Adj# #N#"],
+    	"VP": ["#Vtrans# #NP#", "#Vintr#"],
+    	"Det": ["The", "This", "That"],
+    	"N": ["John Keating", "Bob Harris", "Bruce Wayne", "John Constantine", "Tony Stark", "John Wick", "Sherlock Holmes", "King Leonidas"],
+    	"Adj": ["cool", "lazy", "amazed", "sweet"],
+    	"Vtrans": ["computes", "examines", "helps", "prefers", "sends", "plays with", "messes up with"],
+    	"Vintr": ["coughs", "daydreams", "whines", "slobbers", "appears", "disappears", "exists", "cries", "laughs"]
     }
 
-    setTimeout(function() {
-        loadGrammar("scifi");
+    let grammar = tracery.createGrammar(data);
+    let expansion = grammar.flatten('#start#');
 
-    }, 10);
+    tweets.push(expansion);
 
-    $('#grammarSelect').on('change', function() {
-        loadGrammar(this.value);
-    });
-});
+    printTweet(tweets);
+}
+
+function printTweet(tweets) {
+    let textBox = document.getElementById("tweet");
+    textBox.innerHTML = "";
+    for(let i=tweets.length-1; i>=0; i--) {
+        textBox.innerHTML += "<p>"+tweets[i]+"</p>"
+    }
+}
